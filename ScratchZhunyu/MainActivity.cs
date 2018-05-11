d57ec759176b7d19a54f6f5b675f86e8eb76d396
Владимирович Иван <van8tka@tut.by>
2018-05-11 22:51:22 +0300

    Add project files.

---
+++ ScratchZhunyu/MainActivity.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Media;
+
+namespace ScratchZhunyu
+{
+	[Activity(Label = "Погладь Жуню", Theme = "@style/AppTheme.NoActionBar")]
+	public class MainActivity : AppCompatActivity, View.IOnTouchListener
+	{
+     
+        protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.activity_main);
+            var imgScratch = FindViewById<View>(Resource.Id.forTapCat);
+            imgScratch.SetOnTouchListener(this);          
+    }
+
+        protected MediaPlayer player;
+       
+
+
+        bool isPlay = false;
+
+        public bool OnTouch(View v, MotionEvent e)
+        {
+            if(e.Action == MotionEventActions.Move)
+            {
+                if(!isPlay)
+                {
+                    var afd = Assets.OpenFd("myr.mp3");
+                    if (player == null)
+                    {
+                        player = new MediaPlayer();
+                       
+                    }
+                    player.Reset();
+                    player.SetDataSource(afd.FileDescriptor, afd.StartOffset, afd.Length);
+                    player.Prepare();
+                    player.Looping = true;
+                    player.Start();
+
+                }
+                   
+                isPlay = true;
+            }
+            if(e.Action == MotionEventActions.Up)
+            {
+                if(player!=null)
+                    player.Stop();
+                isPlay = false;
+            }
+            return true;
+        }
+
+
+        public override void OnBackPressed()
+        {
+            if (player != null)
+                player.Stop();
+            base.OnBackPressed();
+        }
+    }
+}
+
---
+++ ScratchZhunyu/SplashActivity.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace ScratchZhunyu
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+       
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+
+        async void SimulateStartup()
+        {
+            await Task.Delay(3000);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+
+    }
+}
